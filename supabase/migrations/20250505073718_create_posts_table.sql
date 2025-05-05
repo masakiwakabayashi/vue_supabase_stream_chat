cf3a0ce2570f5e65e9c7231fbf5e1dbd64a21e43
masakiwakabayashi <masaki.wakabaya@gmail.com>
2025-05-05 16:39:16 +0900

    create post table migration

---
+++ supabase/migrations/20250505073718_create_posts_table.sql
@@ -0,0 +1,58 @@
+create extension if not exists "vector" with schema "public" version '0.8.0';
+
+create table "public"."posts" (
+    "id" uuid not null default gen_random_uuid(),
+    "content" text not null,
+    "embedding" vector(1536),
+    "created_at" timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text),
+    "updated_at" timestamp with time zone not null default (now() AT TIME ZONE 'utc'::text)
+);
+
+
+CREATE UNIQUE INDEX posts_pkey ON public.posts USING btree (id);
+
+alter table "public"."posts" add constraint "posts_pkey" PRIMARY KEY using index "posts_pkey";
+
+grant delete on table "public"."posts" to "anon";
+
+grant insert on table "public"."posts" to "anon";
+
+grant references on table "public"."posts" to "anon";
+
+grant select on table "public"."posts" to "anon";
+
+grant trigger on table "public"."posts" to "anon";
+
+grant truncate on table "public"."posts" to "anon";
+
+grant update on table "public"."posts" to "anon";
+
+grant delete on table "public"."posts" to "authenticated";
+
+grant insert on table "public"."posts" to "authenticated";
+
+grant references on table "public"."posts" to "authenticated";
+
+grant select on table "public"."posts" to "authenticated";
+
+grant trigger on table "public"."posts" to "authenticated";
+
+grant truncate on table "public"."posts" to "authenticated";
+
+grant update on table "public"."posts" to "authenticated";
+
+grant delete on table "public"."posts" to "service_role";
+
+grant insert on table "public"."posts" to "service_role";
+
+grant references on table "public"."posts" to "service_role";
+
+grant select on table "public"."posts" to "service_role";
+
+grant trigger on table "public"."posts" to "service_role";
+
+grant truncate on table "public"."posts" to "service_role";
+
+grant update on table "public"."posts" to "service_role";
+
+
